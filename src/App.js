dddd76ffde4fb4d04655a7b26d6912095f45c8df
aktarinaakhi <aktarinacsehstu@gmail.com>
2021-12-16 10:29:22 +0600

    added login page

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NotFound from './pages/NotFound/NotFound';
 import Home from './pages/Home/Home';
 import AuthProvider from './context/AuthProvider';
+import Navigation from './shared/Navigation';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        {/* <Header></Header> */}
+        <Navigation />
         <Routes>
 
-          <Route path="/" element={<Home />}> </Route>
-
-
+          <Route exact path="/" element={<Home />}> </Route>
+          <Route exact path="/home" element={<Home />}> </Route>
+          <Route exact path="/login" element={<Login />}> </Route>
+          <Route exact path="/register" element={<Register />}> </Route>
 
           <Route path="*" element={<NotFound />}> </Route>
 
